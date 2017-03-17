0188103e1cae15d600693871c4560b259da52fd2
otaiga <karl.turner5@gmail.com>
2017-03-17 14:14:53 +0000

    included handlebars for template

--- public/js/custom.js
+++ public/js/custom.js
@@ -1,13 +1,27 @@
-/* global window, google MarkerClusterer document aeds window */
+/* global window, google MarkerClusterer document aeds window Handlebars */
 
 'use strict';
 
 (function init() {
+  function createInfoContent(location) {
+    const template = document.getElementById('aedTemplate').innerHTML;
+    const compiledTemplate = Handlebars.compile(template);
+    return compiledTemplate(location);
+  }
+
   function addAeds(map, aedData) {
-    const markers = aedData.map(location =>
-      new google.maps.Marker({
+    const markers = aedData.map((location) => {
+      const infowindow = new google.maps.InfoWindow({
+        content: createInfoContent(location),
+      });
+      const marker = new google.maps.Marker({
         position: { lat: location.latitude, lng: location.longitude },
-      }));
+      });
+      marker.addListener('click', () => {
+        infowindow.open(map, marker);
+      });
+      return marker;
+    });
     /* eslint no-new: 0 */
     new MarkerClusterer(map, markers, { imagePath: '/images/m' });
   }
